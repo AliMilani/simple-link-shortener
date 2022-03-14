d80740ade5ce18b9f38a2935bc74ed715e551a8b
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-03-14 17:47:31 +0330

    Update env example for jwt secret key.

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ mongoose
     .catch((err) => console.log(err));
 
 // in windows:  $ENV:shortner_jwtPrivateKey = 'key'
+// or set in .env file
 if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
 }
